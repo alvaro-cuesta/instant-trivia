14da69dc610deb96f27be6b15482f42e5bf84eaa
Álvaro Cuesta <alvaro-cuesta@GitHub>
2018-08-22 02:12:59 +0200

    fix indentation

--- src/presentational/ScrollLayout.jsx
+++ src/presentational/ScrollLayout.jsx
@@ -4,18 +4,18 @@ import "./ScrollLayout.css";
 
 const ScrollLayout = ({ current, children, onTransitionEnd}) => (
   <div className='scroll-container'>
-  <div
-    className="scroll-content"
-    style={{
-      transform: `translateY(-${(100 * current) /
-        React.Children.count(children)}%)`
-    }}
-    onTransiionEnd={onTransitionEnd}
-  >
-    {React.Children.map(children, child => (
-      <div className="scroll-panel">{child}</div>
-    ))}
-  </div>
+    <div
+      className="scroll-content"
+      style={{
+        transform: `translateY(-${(100 * current) /
+          React.Children.count(children)}%)`
+      }}
+      onTransitionEnd={onTransitionEnd}
+    >
+      {React.Children.map(children, child => (
+        <div className="scroll-panel">{child}</div>
+      ))}
+    </div>
   </div>
 );
 
