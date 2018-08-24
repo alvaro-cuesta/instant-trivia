a5a336b019904903c2b52799037cfdc08628c8fc
Álvaro Cuesta <alvaro-cuesta@GitHub>
2018-08-24 20:51:07 +0200

    move App to its own file, fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,12 @@ const paths = {
   output: path.join(__dirname, 'build'),
 };
 
-module.exports = require('@alvaro-cuesta/webpack-parts').spa({
-  name: 'Instant Trivia',
-  paths,
-  alias: {
-    presenational: path.join(paths.app, 'presenational'),
-    smart: path.join(paths.app, 'smart'),
-  },
-});
+module.exports = require('@alvaro-cuesta/webpack-parts')
+  .spa({
+    name: 'Instant Trivia',
+    paths,
+    alias: {
+      presentational: path.join(paths.app, 'presentational'),
+      smart: path.join(paths.app, 'smart'),
+    },
+  });
